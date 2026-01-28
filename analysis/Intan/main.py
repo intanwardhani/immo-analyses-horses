25e27bdfe65adcc837697e50a37eae8282f8f8c6
I. K. Wardhani <67377763+intanwardhani@users.noreply.github.com>
2026-01-28 15:52:37 +0100

    Update CSV file path to use home directory

--- analysis/Intan/main.py
+++ analysis/Intan/main.py
@@ -2,7 +2,7 @@ from file_utils import FileManager
 
 def main():
     fm = FileManager()                
-    df = fm.get_csv("/Users/intankwardhani/Documents/BeCode/immo-eliza-team-horses-analysis/data/properties_data_original.csv")
+    df = fm.get_csv("~/data/properties_data_original.csv")
     print(df)
 
 if __name__ == "__main__":
